b2d56a13031b0510639b5fbd6a26dfbd50ed303e
Maximiliano Herrera <herreramaximiliano@hotmail.com>
2024-11-03 21:34:10 -0300

    se creo archivo multiplication

---
+++ createMultiplicacionTable.js
@@ -0,0 +1,9 @@
+function createMultiplicationTable(params) {
+    
+    for (let i = 0; i <= 10; i++) {
+        console.log(`1 * ${i} = ${1*i}`);
+        
+    }
+}
+
+console.log(createMultiplicationTable());
